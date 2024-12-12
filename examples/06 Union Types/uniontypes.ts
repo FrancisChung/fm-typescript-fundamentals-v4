cac75291d829aa7fc8f280af4b8e9c81a1360490
Francis Chung <francis.c.chung@gmail.com>
2024-12-12 16:16:20 +1100

    Demo of Union Types Part 2

--- examples/06 Union Types/uniontypes.ts
+++ examples/06 Union Types/uniontypes.ts
@@ -6,4 +6,20 @@ type Evens = 2 | 4 | 6| 8
 let evenNumber : Evens = 2
 evenNumber = 5 // 5 is not part of set
 
-let evenOrLowNumber = 5 as Evens | OneThroughFive
+//let evenOrLowNumber = 5 as Evens | OneThroughFive
+
+function printEven(even : Evens) {}
+function printLowNumber(lowNumber : OneThroughFive) {}
+function printEvenNumberUnder5(num : 2 | 4) : void {}
+function printNumber (num : Number) : void {}
+
+let evenOrLowNumber : Evens | OneThroughFive
+// evenOrLowNumber =6
+// evenOrLowNumber = 3
+// evenOrLowNumber = 4
+let x = 5 as Evens | OneThroughFive
+
+printEven(x)        // 1 is not assignable to type Evens
+printLowNumber(x)   // 8 is not assignable to OneThroughFive
+printEvenNumberUnder5(x)    // Type 1 is not assignable to 2 | 4
+printNumber (x) // Ok
