4e0bc3859accbcafa7697eb959c4db4a897ace65
Francis Chung <francis.c.chung@gmail.com>
2024-12-16 12:45:49 +1100

    Commented out code that was giving errors

--- examples/06 Union Types/uniontypes.ts
+++ examples/06 Union Types/uniontypes.ts
@@ -1,10 +1,10 @@
 type OneThroughFive = 1 | 2 | 3 | 4 | 5
 let lowNumber : OneThroughFive = 3
-lowNumber = 8 // invalid
+lowNumber = 5 // invalid
 
 type Evens = 2 | 4 | 6| 8
 let evenNumber : Evens = 2
-evenNumber = 5 // 5 is not part of set
+evenNumber = 2 // 5 is not part of set
 
 //let evenOrLowNumber = 5 as Evens | OneThroughFive
 
@@ -19,7 +19,7 @@ let evenOrLowNumber : Evens | OneThroughFive
 // evenOrLowNumber = 4
 let x = 5 as Evens | OneThroughFive
 
-printEven(x)        // 1 is not assignable to type Evens
-printLowNumber(x)   // 8 is not assignable to OneThroughFive
-printEvenNumberUnder5(x)    // Type 1 is not assignable to 2 | 4
-printNumber (x) // Ok
+// printEven(x)        // 1 is not assignable to type Evens
+// printLowNumber(x)   // 8 is not assignable to OneThroughFive
+// printEvenNumberUnder5(x)    // Type 1 is not assignable to 2 | 4
+// printNumber (x) // Ok
