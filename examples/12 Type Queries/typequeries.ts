2ef0a230590bfeec66a3eeb1c2a992262b84d85e
Francis Chung <francis.c.chung@gmail.com>
2024-12-19 15:03:31 +1100

    Type Queries, typeof operator

--- examples/12 Type Queries/typequeries.ts
+++ examples/12 Type Queries/typequeries.ts
@@ -3,7 +3,8 @@ type DatePropertyNames = keyof Date
 type DateStringPropertyNames = keyof DatePropertyNames & string
 type DateSymbolPropertyNames = keyof DatePropertyNames & symbol
 
-// typeof
+// keyof = Object.keys() for types
+// typeof = "get me the type of this value:
 
 async function main() {
     const apiResponse = await Promise.all([
@@ -22,3 +23,21 @@ CSSRule.STYLE_RULE
 const fooRule = new MyRule()
 
 type MyRuleType = typeof MyRule
+
+// Indexed Access Types
+
+interface Car {
+    make: string
+    model: string
+    year: number
+    color: {
+        red: string
+        green: string
+        blue: string
+    }
+}
+
+let carColor: Car["color"]
+let carSomething: Car["Not-in-the-car"]
+let carColorRedComponent: Car["color"]["red"]
+let carProperty: Car["color" | "year"]
