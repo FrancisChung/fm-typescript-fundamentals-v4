cdabbb23899f5a326092c9fb5d6bd6ef23fadf05
Francis Chung <francis.c.chung@gmail.com>
2024-12-20 11:42:53 +1100

    Initial Version

---
+++ examples/14 Callables/callables.ts
@@ -0,0 +1,11 @@
+// Callables
+
+interface TwoNumberCalculation {
+    (x: number, y:number) : number
+}
+
+type TwoNumberCalc = (x: number, y:number) => number
+
+const add: TwoNumberCalculation = (a, b) => a + b
+const subtract: TwoNumberCaclc = (x, y) => x - y
+s
