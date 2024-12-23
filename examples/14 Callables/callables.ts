82a7d3e293930dc87987f361e543b1116bf44180
Francis Chung <francis.c.chung@gmail.com>
2024-12-23 13:19:12 +1100

    Void vs Undefined example

--- examples/14 Callables/callables.ts
+++ examples/14 Callables/callables.ts
@@ -11,3 +11,14 @@ type TwoNumberCalc = (x: number, y:number) => number
 const add: TwoNumberCalculation = (a, b) => a+b
 const subtract: TwoNumberCalc = (x: number, y: number) => x-y
 
+function invokeInFourSeconds(callback:() => undefined) {
+    setTimeout(callback, 4000)
+}
+
+function invokeInFiveSeconds(callback:() => void) {
+    setTimeout(callback, 5000)
+}
+
+const values: number[] = []
+// invokeInFourSeconds(()=> values.push(4)) // Error : number is not assignable to type undefined
+invokeInFiveSeconds(()=> values.push(4))
