48a0985bc164c0dfd1d07e3c0006b12a1e8c0fc5
Francis Chung <francis.c.chung@gmail.com>
2024-12-23 13:56:02 +1100

    Intial Version

---
+++ examples/16 Constructables/constructables.ts
@@ -0,0 +1,22 @@
+// Constructables
+
+interface DateConstructor {
+    new(value: number): Date
+}
+
+let myDateConstructor : DateConstructor = Date
+const d = new myDateConstructor(1697923072611)
+
+type FormSubmitHandler = (data : FormData) => void
+type MessageHandler = (event : MessageEvent) => void
+
+function handleMainEvent (
+    elem : HTMLFormElement | HTMLIFrameElement,
+    handler : FormSubmitHandler | MessageHandler
+) {}
+
+const myFrame = document.getElementsByTagName("iframe")[0]
+
+// handleMainEvent(myFrame, (val ) => {
+//
+// })
