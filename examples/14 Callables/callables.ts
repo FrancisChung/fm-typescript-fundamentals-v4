44c6037607f67f2f8fe99a4880aef5185e5aa743
Francis Chung <francis.c.chung@gmail.com>
2024-12-23 12:47:09 +1100

    Added notes and bugfix

--- examples/14 Callables/callables.ts
+++ examples/14 Callables/callables.ts
@@ -3,9 +3,11 @@
 interface TwoNumberCalculation {
     (x: number, y:number) : number
 }
+// imterface use a colom
+// type alias use a fat arrow
 
 type TwoNumberCalc = (x: number, y:number) => number
 
 const add: TwoNumberCalculation = (a, b) => a+b
-const subtract: TwoNumberCaclc = (x, y) => x-y
+const subtract: TwoNumberCalc = (x: number, y: number) => x-y
 
